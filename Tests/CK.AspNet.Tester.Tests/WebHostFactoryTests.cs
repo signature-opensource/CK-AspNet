dcbc23b751f29bb50df47a8918f6c2f7e8411cab
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-04-16 19:12:07 +0200

    Added a simple test.

---
+++ Tests/CK.AspNet.Tester.Tests/WebHostFactoryTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Net;
+
+namespace CK.AspNet.Tester.Tests
+{
+    [TestFixture]
+    public class WebHostFactoryTests
+    {
+
+        public class StupidService
+        {
+            public string GetText() => $"It is {DateTime.UtcNow}.";
+        }
+
+        public class StupidMiddleware
+        {
+            readonly RequestDelegate _next;
+            readonly StupidService _s;
+
+            public StupidMiddleware(RequestDelegate next, StupidService s )
+            {
+                _next = next;
+                _s = s;
+            }
+
+            public Task Invoke(HttpContext context)
+            {
+                if( context.Request.Query.ContainsKey("sayHello") )
+                {
+                    context.Response.StatusCode = StatusCodes.Status200OK;
+                    return context.Response.WriteAsync("Hello! " + _s.GetText());
+                }
+                return _next.Invoke(context);
+            }
+        }
+
+        [Test]
+        public void hello_world_midleware()
+        {
+            var b = WebHostBuilderFactory.Create(null, null,
+                services =>
+                {
+                    services.AddSingleton<StupidService>();
+                },
+                app =>
+                {
+                    app.UseMiddleware<StupidMiddleware>();
+                });
+            var server = new TestServer(b);
+            var client = new TestClient(server);
+
+            HttpResponseMessage notFound = client.Get("other");
+            Assert.That(notFound.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+
+            HttpResponseMessage hello = client.Get("?sayHello");
+            Assert.That(hello.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            var content = hello.Content.ReadAsStringAsync().Result;
+            Assert.That(content.StartsWith("Hello! ") );
+        }
+    }
+}
