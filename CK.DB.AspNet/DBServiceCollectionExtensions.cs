23f7e8875d2de6b6bbca4aba983f31bd8fd09e5e
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-04-17 14:27:54 +0200

    New CK.DB.AspNet project.

---
+++ CK.DB.AspNet/DBServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using CK.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CK.AspNet
+{
+    /// <summary>
+    /// Adds extension methods on <see cref="IServiceCollection"/>.
+    /// Since the extension methods here do not conflict with more generic methods, the namespace is
+    /// CK.AspNet to avoid cluttering the namespace names.
+    /// </summary>
+    public static class DBServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers all the StObj mappings from the default context of an assembly.
+        /// </summary>
+        /// <param name="services">This services.</param>
+        /// <param name="assemblyName">The assembly name.</param>
+        /// <returns>This services collection.</returns>
+        public static IServiceCollection AddDefaultStObjMap(this IServiceCollection services, string assemblyName)
+        {
+            return AddStObjMap(services, StObjContextRoot.Load(assemblyName).Default);
+        }
+
+        /// <summary>
+        /// Registers all the StObj mappings from a StObj context.
+        /// </summary>
+        /// <param name="services">This services.</param>
+        /// <param name="map">Contextual StObj objects to register.</param>
+        /// <returns>This services collection.</returns>
+        public static IServiceCollection AddStObjMap(this IServiceCollection services, IContextualStObjMap map)
+        {
+            if (map == null) throw new ArgumentNullException(nameof(map));
+            foreach (var kv in map.Mappings)
+            {
+                services.AddSingleton(kv.Key, kv.Value);
+            }
+            return services;
+        }
+
+    }
+}
