13476f9d365f64710b302df19d2ece4d549a38fe
Cédric Legendre <albundy1385@gmail.com>
2017-11-03 11:00:21 +0100

    Add new extensions methods that accepts AssemblyName and Assembly and LoadFrom
    on NetCore.

--- CK.DB.AspNet/DBServiceCollectionExtensions.cs
+++ CK.DB.AspNet/DBServiceCollectionExtensions.cs
@@ -23,11 +23,16 @@ namespace CK.AspNet
         /// Optional connection string that will override <see cref="SqlDefaultDatabase"/> <see cref="SqlDatabase.ConnectionString">ConnectionString</see>.
         /// </param>
         /// <returns>This services collection.</returns>
-        public static IServiceCollection AddDefaultStObjMap( this IServiceCollection services, string assemblyName, string defaultConnectionString = null )
+        public static IServiceCollection AddDefaultStObjMap( this IServiceCollection services, Assembly stobjAssembly, string defaultConnectionString = null )
         {
-            var map = StObjContextRoot.Load( Assembly.Load( new AssemblyName( assemblyName ) ) );
+            if( stobjAssembly == null )
+            {
+                throw new ArgumentNullException( nameof( stobjAssembly ) );
+            }
+
+            var map = StObjContextRoot.Load( stobjAssembly );
             if( map == null )
-                throw new ArgumentException( $"The assembly {assemblyName} was not found or is not a valid StObj map assembly" );
+                throw new ArgumentException( $"The assembly {stobjAssembly.FullName} was not found or is not a valid StObj map assembly" );
 
             if( !String.IsNullOrEmpty( defaultConnectionString ) )
             {
@@ -35,6 +40,44 @@ namespace CK.AspNet
                 db.ConnectionString = defaultConnectionString;
             }
             return AddStObjMap( services, map.Default );
+        }
+
+        /// <summary>
+        /// Registers all the StObj mappings from the default context of an assembly and also
+        /// registers the <see cref="IStObjMap"/>.
+        /// </summary>
+        /// <param name="services">This services.</param>
+        /// <param name="assemblyName">The assembly name.</param>
+        /// <param name="defaultConnectionString">
+        /// Optional connection string that will override <see cref="SqlDefaultDatabase"/> <see cref="SqlDatabase.ConnectionString">ConnectionString</see>.
+        /// </param>
+        /// <remarks>
+        /// On NetCore runtime,  Assembly.LoadFrom is used to resolves the assembly from its full path.
+        /// </remarks>
+        /// <returns>This services collection.</returns>
+        public static IServiceCollection AddDefaultStObjMap( this IServiceCollection services, string assemblyName, string defaultConnectionString = null )
+        {
+#if NET461
+            return services.AddDefaultStObjMap( new AssemblyName( assemblyName ), defaultConnectionString );
+#else
+            string path = System.IO.Path.Combine( AppDomain.CurrentDomain.BaseDirectory, assemblyName + ".dll" );
+            return services.AddDefaultStObjMap( Assembly.LoadFrom( path ), defaultConnectionString );
+#endif
+        }
+
+        /// <summary>
+        /// Registers all the StObj mappings from the default context of an assembly and also
+        /// registers the <see cref="IStObjMap"/>.
+        /// </summary>
+        /// <param name="services">This services.</param>
+        /// <param name="assemblyName">The assembly name.</param>
+        /// <param name="defaultConnectionString">
+        /// Optional connection string that will override <see cref="SqlDefaultDatabase"/> <see cref="SqlDatabase.ConnectionString">ConnectionString</see>.
+        /// </param>
+        /// <returns>This services collection.</returns>
+        public static IServiceCollection AddDefaultStObjMap( this IServiceCollection services, AssemblyName assemblyName, string defaultConnectionString = null )
+        {
+            return services.AddDefaultStObjMap( Assembly.Load( assemblyName ), defaultConnectionString );
         }
 
         /// <summary>
